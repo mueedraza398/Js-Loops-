ba6281c2c416211246e2c9f97a959ebdddee0e9c
Abdul Mueed <mueedraza398@gmail.com>
2025-06-24 12:34:58 +0500

    HTML + JAVASCRIPT

---
+++ script.js
@@ -0,0 +1,134 @@
+// const fruits = ["Apple", "Banana", "Mango"];
+// const fruitList = document.getElementById("fruit-list");
+// fruits.forEach(fruit => {
+//   const li = document.createElement("li");
+//   li.textContent = fruit;
+//   fruitList.appendChild(li);
+// });
+
+
+// const quotes = ["Quote 1", "Quote 2", "Quote 3"];
+// const container = document.getElementById("quotes-container");
+// for (let i = 0; i < quotes.length; i++) {
+//   const p = document.createElement("p");
+//   p.textContent = quotes[i];
+//   container.appendChild(p);
+// }
+
+
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
+// const ol = document.getElementById("number-list");
+// numbers.forEach(num => {
+//   const li = document.createElement("li");
+//   li.textContent = num;
+//   ol.appendChild(li);
+// });
+
+
+// const ages = [12, 25, 30, 17];
+// const ageList = document.getElementById("age-list");
+// ages.forEach(age => {
+//   if (age > 18) {
+//     const li = document.createElement("li");
+//     li.textContent = age;
+//     ageList.appendChild(li);
+//   }
+// });
+
+
+
+
+
+// const colors = ["Red", "Blue", "Green"];
+// const colorList = document.getElementById("color-list");
+// colors.forEach((color, index) => {
+//   const li = document.createElement("li");
+//   li.textContent = `Index ${index}: ${color}`;
+//   colorList.appendChild(li);
+// });
+
+
+
+
+
+
+// const tasks = ["Wake up", "Study", "Sleep"];
+// const taskList = document.getElementById("task-list");
+// for (let i = tasks.length - 1; i >= 0; i--) {
+//   const li = document.createElement("li");
+//   li.textContent = tasks[i];
+//   taskList.appendChild(li);
+// }
+
+
+
+
+
+// const comments = ["Good work", "Nice page", "Well done!"];
+// const commentSection = document.getElementById("comment-section");
+// comments.forEach(c => {
+//   const p = document.createElement("p");
+//   p.textContent = c;
+//   commentSection.appendChild(p);
+// });
+
+
+
+
+
+
+
+
+
+// const scores = [70, 85, 90, 60];
+// const ul = document.getElementById("score-list");
+// scores.forEach(score => {
+//   if (score > 80) {
+//     const li = document.createElement("li");
+//     li.textContent = score;
+//     ul.appendChild(li);
+//   }
+// });
+
+
+
+
+
+// const scores = [70, 85, 90, 60];
+// const ul = document.getElementById("score-list");
+// scores.forEach(score => {
+//   if (score > 80) {
+//     const li = document.createElement("li");
+//     li.textContent = score;
+//     ul.appendChild(li);
+//   }
+// });
+
+
+
+
+// const countries = ["Pakistan", "India", "USA"];
+// const select = document.getElementById("country-select");
+// countries.forEach(country => {
+//   const option = document.createElement("option");
+//   option.textContent = country;
+//   select.appendChild(option);
+// });
+
+
+
+
+
+// const temps = [25, 32, 28, 40];
+// const tempList = document.getElementById("temp-list");
+// temps.forEach(temp => {
+//   const li = document.createElement("li");
+//   li.textContent = temp + "°C";
+//   if (temp > 30) li.style.color = "red";
+//   tempList.appendChild(li);
+// });
+
+
+
+
